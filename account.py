eb5af59f709279fc5f0a2834078855545be329d4
Chukwunonso David Ilonze <nonsoilonze@gmail.com>
2023-01-24 09:49:01 +0100

    Update account.py
    doc

--- account.py
+++ account.py
@@ -12,6 +12,15 @@ class Account:
     """
 
     def __init__(self, deposits, withdrawals):
+        """_summary_
+
+        Args:
+            deposits (_type_): _description_
+            withdrawals (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
         if type(deposits) == list and type(withdrawals) == list:
             self.deposits           = deposits
             self.withdrawals        = withdrawals
@@ -21,11 +30,26 @@ class Account:
             return 'error! not a list'
 
     def balance(self):
+        """_summary_
+
+        Returns:
+            _type_: float balance
+        """
         self.balance = self.total_deposit - self.total_withdrawal
         return (self.balance)
 
     def correct_balance(self, supplied_balance):
-            if supplied_balance == self.balance:
-                return True
-            else:
-                return False
+        """
+        To check if the currrent balance is correct
+        supply the balance as float.
+
+        Args:
+            supplied_balance (_type_): float value
+
+        Returns:
+            _type_: boolean 
+        """
+        if supplied_balance == self.balance:
+            return True
+        else:
+            return False
